462693e5c06308613810568fc01615db24b706f3
ShaileshWattamwar <144618665+ShaileshWattamwar@users.noreply.github.com>
2023-09-13 12:06:41 +0530

    Update Lengthmethod.java

--- Lengthmethod.java
+++ Lengthmethod.java
@@ -7,7 +7,7 @@ package TDIT_String;
 
             String Line = "Java is fun but we have learn the basics first";
 
-            // returns the length of str1
+            
             int length = Line.length();
 
             System.out.println(Line.length());
